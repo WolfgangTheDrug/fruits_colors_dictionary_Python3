03cf6430172cc4cf4f3050d7f281c9171fb228f9
Krzysztof Kaminski <kkam@matrix.umcs.pl>
2021-06-25 23:08:06 +0200

    server can now search through the database

--- combine_json_dictionaries.py
+++ combine_json_dictionaries.py
@@ -1,14 +1,19 @@
 import glob
 import os
+import sys
 
 if os.path.exists('dictionary.json'):
-	os.remove('dictionary.json')									#make sure you won't append new data to old one
+	os.remove('dictionary.json')													#make sure you won't append new data to old one
+
+DUMMY_EOF = '\t{\n\t\t\"eng\": \"\",\n\t\t"pol": \"\"\n\t}\n'						#quick solution to all json library problems
 
 try:
-	with open('dictionary.json', 'a') as dic:							#open base file
-		for file in glob.iglob('*.json'):								#serch for .json files 
-			if file.find('_') > 0:										#take only those that have underscore somewhere in the middle of its name
-				with open(file, 'r') as source:							#open each one of them
-					[dic.write(line) for line in source.readlines()]	#write their contents to the base file
-except e:
-	print('An exception occurred', file=stderr)
+	with open('dictionary.json', 'a') as dic:										#open base file
+		dic.write('[\n')
+		for file in glob.iglob('*.json'):											#serch for .json files 
+			if file.find('_') > 0:													#take only those that have underscore somewhere in the middle of its name
+				with open(file, 'r') as source:										#open each one of them
+					[dic.write('\t{}'.format(line)) for line in source.readlines()]	#write their contents to the base file
+		dic.write(DUMMY_EOF + ']')
+except Exception as e:
+	print('An exception occurred: {}'.format(e))
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import argparse, socket, sys
-import struct, signal
+import signal
 import glob, os
+import json
 
 parser = argparse.ArgumentParser(description='Pol-Eng Fruits & Colors Dictionary Server')
 parser.add_argument('port',
@@ -18,11 +19,33 @@ def exit_handler(signum, frame):
 BUFFER_SIZE = 512
 PORT = args.port
 
+class color:
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
+
 # # #
 
 signal.signal(signal.SIGINT, exit_handler)
 
 # # #
+
+# # #
+
+try:
+	with open('dictionary.json') as jsondata:
+		database = json.load(jsondata)
+except Exception as e:
+	print(e)
+	sys.exit(2)
+
 
 with socket.socket() as sock:
 	sock.bind(('', PORT))
@@ -34,12 +57,11 @@ with socket.socket() as sock:
 		print('Connected with ', address)
 
 		while data: 
-			request = data.decode()
-			print('Word requested: ', request)
+			word = data.decode()
 			
-			answer = 'I\'ve received: ' + request
-			connected_socket.sendall(answer.encode())
-
+			[connected_socket.sendall("\nThe word {} can be translated from polish to english and means {}\n".format(color.RED + i['pol'] + color.END, color.BLUE + i['eng'] + color.END).encode()) for i in database if i['pol'] == word]
+			[connected_socket.sendall("\nThe word {} can be translated from english to polish and means {}\n".format(color.RED + i['eng'] + color.END, color.BLUE + i['pol'] + color.END).encode()) for i in database if i['eng'] == word]
+			
 			data = connected_socket.recv(BUFFER_SIZE)
 			
 		connected_socket.shutdown(socket.SHUT_RDWR)
